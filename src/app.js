8ed064a60c71efe8b1f1ee2f77476d5592af6604
VeronicaCastroMurillo <119750643+VeronicaCastroMurillo@users.noreply.github.com>
2023-06-15 18:22:47 +0000

    excuse generator terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = function frases() {
+    let who = ['My dog', 'My grandma', 'I', 'My bird'];
+    let action = ['ate', 'forgot', 'needed to take', 'peed'];
+    let what = ['my homework', 'the keys', 'a mental health break', 'the car'];
+    let when = ['before the class', 'right on time', 'today', 'during my lunch'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    let randomNum = Math.floor(Math.random() * 4);
+   
+    const para = document.createElement("h1");
+    const node = document.createTextNode(who[randomNum]+ " " + action[randomNum]+ " " + what[randomNum] +" "+ when[randomNum]);
+    para.appendChild(node);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    const element = document.getElementById("div1");
+    const child = document.getElementById("p2");
+    element.insertBefore(para, child);
+     
+  };
