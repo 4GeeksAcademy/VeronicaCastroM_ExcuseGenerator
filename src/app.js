c75b7dced078b914a309e0aad16c3536f91ba7a8
VeronicaCastroMurillo <119750643+VeronicaCastroMurillo@users.noreply.github.com>
2023-06-17 02:09:41 +0000

    ordené el código y renombré las variables

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 window.onload = function frases() {
-    let who = ['My dog', 'My grandma', 'I', 'My bird'];
-    let action = ['ate', 'forgot', 'needed to take', 'peed'];
-    let what = ['my homework', 'the keys', 'a mental health break', 'the car'];
-    let when = ['before the class', 'right on time', 'today', 'during my lunch'];
+  let who = ['My dog', 'My grandma', 'I', 'My bird'];
+  let action = ['ate', 'forgot', 'needed to take', 'peed'];
+  let what = ['my homework', 'the keys', 'a mental health break', 'the car'];
+  let when = ['before the class', 'right on time', 'today', 'during my lunch'];
 
-    let randomNum = Math.floor(Math.random() * 4);
-   
-    const para = document.createElement("h1");
-    const node = document.createTextNode(who[randomNum]+ " " + action[randomNum]+ " " + what[randomNum] +" "+ when[randomNum]);
-    para.appendChild(node);
+  let randomNumber = Math.floor(Math.random() * 4);
 
-    const element = document.getElementById("div1");
-    const child = document.getElementById("p2");
-    element.insertBefore(para, child);
-     
-  };
+  //Le puse un nombre más específico y claro a mis variables.
+  const new_tag = document.createElement("h1");
+  const text = document.createTextNode(who[randomNumber] + " " + action[randomNumber] + " " + what[randomNumber] + " " + when[randomNumber]);
+  new_tag.appendChild(text);
+
+  const element = document.getElementById("div1");
+  const child = document.getElementById("p2");
+  element.insertBefore(new_tag, child);
+
+};
